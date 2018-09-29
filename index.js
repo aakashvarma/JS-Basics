2aee3a8335d32a83de95483df27bb2d51041aa75
aakashvarma <aakashvarma18@gmail.com>
2018-09-29 13:21:40 +0530

    Objects in javascript

--- index.js
+++ index.js
@@ -7,16 +7,24 @@ console.log(rate)
 
 // Variable declaration
 
-let name = "Varma"; // String
-let age = 21; // value
-let isApproves = true; //Boolean
-let firstName = undefined; // undefined variable used for showing string variable
-let num = null; // null is used to show that the variable is a number type
+let name = "Varma";                 // String
+let age = 21;                       // value
+let isApproves = true;              //Boolean
+let firstName = undefined;          // undefined variable used for showing string variable
+let num = null;                     // null is used to show that the variable is a number type
 
+// Objects
 
+let person = {                      // Note: The curly braces are called object literal
+    name: "Varma",
+    age: 30
+};
+ 
+console.log(person);
+console.log(person.age);            // Dot notation
+console.log(person["name"])         // Bracket Notation
 
-
-
+// Arrays
 
 
 
