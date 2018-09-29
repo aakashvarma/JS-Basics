38df08299a30ad5b474dd7c5b31fbe8d3542d3ff
aakashvarma <aakashvarma18@gmail.com>
2018-09-29 13:06:27 +0530

    variables learned

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('Hello World console');
+// Const and assigning values
+
+let interestrate = 0.4;
+const rate = 1;
+console.log(interestrate)
+console.log(rate)
+
+// Variable declaration
+
+let name = "Varma"; // String
+let age = 21; // value
+let isApproves = true; //Boolean
+let firstName = undefined; // undefined variable used for showing string variable
+let num = null; // null is used to show that the variable is a number type
+
+
+
+
+
+
+
+
+
+
+
+
+
+
