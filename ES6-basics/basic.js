ba70b1c9ce9d3aeba4c5f8303a3314456206810b
aakashvarma <aakashvarma18@gmail.com>
2018-10-30 16:02:00 +0530

    Switch statements added

--- ES6-basics/basic.js
+++ ES6-basics/basic.js
@@ -6,7 +6,18 @@ let nameField = false;
 console.log(typeof(nameField));
 console.log(typeof(String(nameField)));
 
-
+// Switch statements
+let number = 2;
+switch(number){
+    case 1:
+    console.log("This is case 1");
+    break;
+    case 2:
+    console.log("This is case 2");
+    break;
+    default:
+    console.log("This is an unknown case")
+}
 
 
 
