bbafbfd469360fd144ca56417e6132e9689508a7
aakashvarma <aakashvarma18@gmail.com>
2018-12-05 13:07:11 +0530

    Methdod chaining added

--- ES6-basics/objects.js
+++ ES6-basics/objects.js
@@ -16,11 +16,31 @@
 
 class User {
     // first thing to do is to create a constructor function
-    
+    constructor(name, email){
+        this.name = name;
+        this.email = email;
+        this.score = 0;
+    }
+    login(){
+        console.log(this.email, "has logged in");
+        return this;
+    }
+    logout(){
+        console.log(this.email, "has logged out");
+        return this;
+    }
+    updateScore(){
+        this.score++;
+        console.log(this.email, "score is now", this.score);
+        return this;
+    }
 }
 
+let userOne = new User('Varma', "aakashvarma@gmail.com");
+let userTwo = new User('Aakash', "varma@gmail.com");
 
 
+userOne.login().updateScore().updateScore().logout(); // Method chaining. This is done my returning 'this' keyword at the end of the method of each function that you want to chain
 
 
 
