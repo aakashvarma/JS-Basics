82afd01de7c6adaa0f9ef96ab71511e13dbc903b
aakashvarma <aakashvarma18@gmail.com>
2018-12-05 12:11:42 +0530

    classes added

--- ES6-basics/objects.js
+++ ES6-basics/objects.js
@@ -1,19 +1,23 @@
 
-var userOne = {
-    name: "Aakash",
-    email: "aakashvarma18@gmail.com",
-    login(){
-        console.log(this.email, "has logged in");
-    },
-    logout(){
-        console.log(this.email, "has logged out");
-    }
-};
+// var userOne = {
+//     name: "Aakash",
+//     email: "aakashvarma18@gmail.com",
+//     login(){
+//         console.log(this.email, "has logged in");
+//     },
+//     logout(){
+//         console.log(this.email, "has logged out");
+//     }
+// };
+
+// console.log(userOne);
 
 
 
-
-
+class User {
+    // first thing to do is to create a constructor function
+    
+}
 
 
 
