00e183ee75c9d61142dec7780d9ad6756ef64cfc
aakashvarma <aakashvarma18@gmail.com>
2018-12-10 23:31:01 +0530

    servers added

--- Node-basics/client_server/app.js
+++ Node-basics/client_server/app.js
@@ -1,9 +1,11 @@
 let http = require('http');
 
-let server = http.createServer(function(req, res){
-    
-});
-
+http.createServer((req, res) => {
+    console.log("Request was made to: " + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, World!\n');
+}).listen(8124, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:8124');
 
 
 
