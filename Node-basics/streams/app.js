d5457040f0cf9fd7cd676503ecd056ecab1e438a
aakashvarma <aakashvarma18@gmail.com>
2018-12-18 16:40:41 +0530

    write stream added

--- Node-basics/streams/app.js
+++ Node-basics/streams/app.js
@@ -3,17 +3,31 @@
 // Readable stream - allows node js to read from a stream
 // Duplex - can read and write to a stream
 
+
+// Readable stream example
 let http = require('http');
 let fs = require('fs');
 
-let myReadStream = fs.createReadStream(__dirname + '/readme.txt');
+let myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8'); 
+
+// createReadStream inherits from an event emitter called 'data'
 
 myReadStream.on('data', function(chunk){
     console.log('new chunk received');
 });
 
+// Writable stream example
 
+let http = require('http');
+let fs = require('fs');
 
+let myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8'); 
+let myWriteStream = fs.createWriteStream(__dirname + '/writeme.txt');
+
+myReadStream.on('data', function(chunk){
+    console.log('new chunk added');
+    myWriteStream.write(chunk);
+});
 
 
 
