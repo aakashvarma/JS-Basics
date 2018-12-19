22474f0debe9b57b0ffe1e5776fb7f44877a79ad
aakashvarma <aakashvarma18@gmail.com>
2018-12-19 23:40:55 +0530

    simple routing added

--- Node-basics/html_json/app.js
+++ Node-basics/html_json/app.js
@@ -14,29 +14,52 @@
 
 // json 
 
-let http = require('http');
-let fs = require('fs');
+// let http = require('http');
+// let fs = require('fs');
 
-let server = http.createServer((req, res) => {
-    console.log('require was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    let myObj = {
-        name: 'Varma',
-        job: 'student',
-        age: 21
-    };
-    res.end(JSON.stringify(myObj));
+// let server = http.createServer((req, res) => {
+//     console.log('require was made: ' + req.url);
+//     res.writeHead(200, {'Content-Type': 'application/json'});
+//     let myObj = {
+//         name: 'Varma',
+//         job: 'student',
+//         age: 21
+//     };
+//     res.end(JSON.stringify(myObj));
     
-});
+// });
 
-server.listen(3000, '127.0.0.1');
-console.log('listening to port: http://127.0.0.1:3000');
+// server.listen(3000, '127.0.0.1');
+// console.log('listening to port: http://127.0.0.1:3000');
 
 
 // Basic Routing
 
+let http = require('http');
+let fs = require('fs');
+let express = require('express');
 
+let server = http.createServer((req, res) => {
+    if (req.url === '/' || req.url === '/home'){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if(req.url === '/api/varma'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        let myObj = {
+            name: 'Aakash Varma',
+            sex: 'Male',
+            age: 21,
+            occupation: 'student'
+        };
+        res.end(JSON.stringify(myObj));
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end("OOPS!! This page doesnot exist.");
+    }
+});
 
+server.listen(3000, '127.0.0.1');
+console.log('listening to http://127.0.0.1:3000');
 
 
 
