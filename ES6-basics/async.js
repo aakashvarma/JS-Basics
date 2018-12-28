8becda02bdcd5f333e582d6b72613b6788dc0c12
aakashvarma <aakashvarma18@gmail.com>
2018-12-28 23:09:06 +0530

    async/await added

--- ES6-basics/async.js
+++ ES6-basics/async.js
@@ -1,18 +1,29 @@
-console.log('person 1: Shows the ticket.')
-console.log('person 2: Shows the ticket.')
+function doLaundry(){
+    console.log("Laundry Done");
+}
 
-const promiseWifeBringingTicket = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('ticket');
-    }, 3000);
-});
+async function f(){
+    let cleanRoom = new Promise((resolve, reject) => {
+        setTimeout(() => resolve("Cleaning Done"), 2000)
+    });
 
-promiseWifeBringingTicket.then((t) => {
-    console.log(`person 3: Shows the ${t}.`);
-});
+    let result = await cleanRoom;
+    console.log(result);
+    doLaundry();
+}
 
-console.log('person 4: Shows the ticket.')
-console.log('person 5: Shows the ticket.')
+// doLaundry();
+f();
+
+
+
+
+
+
+
+
+
+
 
 
 
